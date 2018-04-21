e7ccfd34ab86bdbed999ad70313ac9565c8d5af8
coltaemanuela <coltaemanuela@gmail.com>
2018-04-21 14:13:24 +0100

    project structure: place config vars in dedicted file

--- index.js
+++ index.js
@@ -4,43 +4,28 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var path = require('path');
 var app = express();
+var config = require('./config/config.json');
 var users = require('./controllers/users');
 
 //setup view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//Firebase initiatialization
-var config = {
-  apiKey: "AIzaSyD2Ch6ki-gZ98roJx7cj6Rcb2-OY4ZM0vo",
-  authDomain: "city-match.firebaseapp.com",
-  databaseURL: "https://city-match.firebaseio.com",
-  projectId: "city-match",
-  storageBucket: "city-match.appspot.com",
-  messagingSenderId: "331136100363"
-};
-firebase.initializeApp(config);
-
-// app.use(session({
-//     secret: "HtpodLPfga5l66rh",
-//     resave:true,
-//     saveUninitialized:true,
-//     cookie:{},
-//     duration: 45 * 60 * 1000,
-//     activeDuration: 15 * 60 * 1000,
-// }));
+//firebase initiatialization
+firebase.initializeApp(config.firebase);
+//user session
+app.use(session(config.session));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//routes
 app.use('/users', users);
-
-
 app.get('/', function (req, res) {
      res.send('City match app rocks!');
 });
 
-//Server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+//server
+app.listen(config.server.port, function () {
+  console.log('City match app listening on port: ' + config.server.port );
 });
