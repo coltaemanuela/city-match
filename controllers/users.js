7ddc5f98f94411dc97c400746e08815970c9f379
E.F.Colta <efc1g17@m19.clients.soton.ac.uk>
2018-05-01 19:20:34 +0100

    form on user profile

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var firebase = require('firebase');
 var router = express.Router();
+var existing_recommendations =[];
 var auth_user = {};
 
 // middleware to verify if user is authenticated. This will secure some routes
@@ -31,15 +32,15 @@ router.get('/register', function(req, res) {
 router.post('/register', function(req, res) {
     //Register a new user with email and password
     firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then(function(data) {
-        // data.sendEmailVerification();
         firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(function(data) {
             firebase.database().ref(`users/${data.uid}`).set({
                 'email': req.body.email,
                 'username': req.body.username?req.body.username:"",
                 'city':req.body.city? req.body.city: "",
-                'age': req.body.age?req.body.age:""
+                'age': req.body.age?req.body.age: 18
             });
         }).then(function (data) {
+						//ask new user to login
             res.redirect("/users/login");
         }).catch(function (err) {
             res.send("error");
@@ -63,7 +64,8 @@ router.post('/login', function(req,res){
         .then(function(details) {
             console.log(firebase.auth().currentUser.email,firebase.auth().currentUser.uid, details.val() );
             res.render('user_profile.ejs', {
-                details: details.val()
+                details: details.val(),
+								existing_recommendations: existing_recommendations
             });
         });
     });
@@ -78,16 +80,35 @@ router.post('/favorite', isAuthenticated,function(req, res){
     });
 });
 
-
 router.post('/review', isAuthenticated,function(req, res){
     console.log('permission approved'+ req.user.uid);
     firebase.database().ref(`users/${req.user.uid}/reviews`).push({
         "city":req.body.city,
-				"title": req.body.title,
-        "reviews_body": req.body.review_body,
+				"title": req.body.review_title,
+        "review_body": req.body.review_body,
         "timestamp": Date.now()
     });
 });
+
+router.post('/recommendations', isAuthenticated, function(req, res){
+	console.log(req.body);
+	console.log(hexToRgbA(req.body.color));
+  console.log('permission approved'+ req.user.uid);
+});
+
+
+function hexToRgbA(hex){
+    var c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+        c= hex.substring(1).split('');
+        if(c.length== 3){
+            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c= '0x'+c.join('');
+        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',1)';
+    }
+    throw new Error('Bad Hex');
+}
 
 
 
