dedfb82518cfbdcef8901b3dc8ba8a627e1ca369
Alejandro Aguilera Ruiz <alejoag9900@gmail.com>
2018-05-04 10:28:03 +0100

    Change data format from object to array, will be easier when plotting the data using d3
    Signed-off-by: Alejandro Aguilera Ruiz <alejoag9900@gmail.com>

--- controllers/compare.js
+++ controllers/compare.js
@@ -32,10 +32,20 @@ function getFavorites(req) {
 
 //returns only cities in both cities and filter
 function buildCities(cities, filter) {
-    var ret = {};
+    var ret = [];
     Object.keys(cities.val()).forEach(function(k) {
         if(filter.indexOf(k) > -1) {
-            ret[k] = cities.val()[k];
+            var c = {};
+            c.city = k;
+            c.Population_2016 = cities.val()[k].Population_2016;
+            c.Employment_Rate_2017 = cities.val()[k].Employment_Rate_2017;
+            c.Average_Weekly_Workplace_Earnings_2017 = cities.val()[k].Average_Weekly_Workplace_Earnings_2017;
+            c.Ratio_of_Private_to_Public_Sector_Employment_2016 = cities.val()[k].Ratio_of_Private_to_Public_Sector_Employment_2016;
+            c.Housing_Affordability_Ratio_2017 = parseFloat(cities.val()[k].Housing_Affordability_Ratio_2017).toFixed(2);
+            c.Mean_house_price_2017 = parseFloat(cities.val()[k].Mean_house_price_2017).toFixed(2);
+            c.CO2_Emissions_per_Capita_2015_tons = parseFloat(cities.val()[k].CO2_Emissions_per_Capita_2015_tons).toFixed(2);
+            c.Ultrafast_Broadband_2017 = cities.val()[k].Ultrafast_Broadband_2017;
+            ret.push(c);
         }
     });
     return ret
