a311529fb84ba7bdba62dc542197a65e21a0a611
coltaemanuela <coltaemanuela@gmail.com>
2018-05-07 23:39:31 +0100

    fix inputs name in POST reviews

--- controllers/cities.js
+++ controllers/cities.js
@@ -3,9 +3,7 @@ var firebase = require('firebase');
 var gcloud = require('google-cloud');
 var config = require('../config/config.json');
 var utils= require('../utils.js');
-var querybase = require('querybase');
 var router = express.Router();
-var auth_user = {};
 
 //storage initialization
 var storage = gcloud.storage({
@@ -13,7 +11,6 @@ var storage = gcloud.storage({
     keyFilename: 'city match-420315c4b0a9.json'
   });
 var bucket = storage.bucket(`city-match.appspot.com`);
-var existing_recommendations =[];
 
 // middleware to verify if user is authenticated. This will secure some routes
 function isAuthenticated (req, res, next) {
@@ -29,12 +26,14 @@ function isAuthenticated (req, res, next) {
 
 router.post('/reviews', isAuthenticated,function(req, res){
     console.log('permission approved'+ req.user.uid);
-    firebase.database().ref(`cities/${req.body.cityName}/reviews`).push({
+    console.log(req.body)
+
+    firebase.database().ref(`cities/${req.body.city}/reviews`).push({
         "userid": req.user.uid,
-        "title": req.body.reviewTitle,
-        "body": req.body.reviewBody,
+        "title": req.body.review_title,
+        "body": req.body.review_body,
         "timestamp": Date.now(),
-        "rating": req.body.reviewRating
+        "rating": req.body.rating
     });
 });
 
